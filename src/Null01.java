268f19a7ab6e6afdbe4b84fb527b8ec0f8d99846
terakawaakari <terakawaakari0701@gmail.com>
2021-08-18 23:04:51 +0900

    first commit

---
+++ src/Null01.java
@@ -0,0 +1,23 @@
+
+public class Null01 {
+	public static void main(String[] srgs) {
+		String string1;
+
+		string1 = "あいう";
+		System.out.println(string1 + "の文字数：" + string1.length());
+
+		string1 = "";
+		System.out.println(string1 + "の文字数：" + string1.length());
+
+		int[] array1 = new int[3];
+		array1[0] = 60;
+		array1[2] = 20;
+		array1[1] = 20;
+		int int2 = array1[2];
+
+//		プリミティブ型の「long」「double」「boolean」の変数に値を代入して出力
+
+//		ラッパークラスの「Float」「Integer」「Character」の変数に値を代入して出力
+//		要素数が5の「int」配列に値を代入して、インデックス番号が3の値を出力
+	}
+}
---
+++ src/Variable01.java
@@ -0,0 +1,26 @@
+
+public class Variable01 {
+	public static void main(String[] args) {
+		int num1;
+		String str1;
+		num1 = 10;
+		str1 = "Hello World!";
+		System.out.println(num1);
+		System.out.println(str1);
+
+		num1 = 20;
+		System.out.println(num1);
+
+		int num2 = 30;
+		System.out.println(num2);
+
+		String name = "山田花子";
+		System.out.println(name);
+
+		name = "山田太郎";
+		System.out.println(name);
+
+		int LACKEY_NUM = 7;
+		System.out.println(LACKEY_NUM);
+	}
+}
