156cde62b178506937ad6877caadce49195ee5b9
terakawaakari <terakawaakari0701@gmail.com>
2021-09-01 02:09:54 +0900

    [Fix]refactoring Q3

--- src/algorithm/Q3.java
+++ src/algorithm/Q3.java
@@ -3,41 +3,70 @@ package algorithm;
 import java.util.Scanner;
 
 public class Q3 {
-	public static void plus(int n1, int n2) {
-		System.out.println(n1 + n2);
-	}
+//	public static void plus(int n1, int n2) {
+//		System.out.println(n1 + n2);
+//	}
+//
+//	public static void minus(int n1, int n2) {
+//		System.out.println(n1 -n2);
+//	}
+//
+//	public static void main(String[] args) {
+//		Scanner scan = new Scanner(System.in);
+//		String f = scan.nextLine();
+//
+//		String[] nums = f.split(" ");
+//		int num1 = Integer.parseInt(nums[0]);
+//		String type1 = nums[1];
+//		int num2 = Integer.parseInt(nums[2]);
+//		String type2 = nums[3];
+//		int num3 = Integer.parseInt(nums[4]);
+//
+//		if (type1.equals("+")) {
+//			int sum = num1 + num2;
+//			if (type2.equals("+")) {
+//				plus(sum, num3);
+//			} else if (type2.equals("-")) {
+//				minus(sum, num3);
+//			}
+//		} else if (type1.equals("-")) {
+//			int sum = num1 - num2;
+//			if (type2.equals("+")) {
+//				plus(sum, num3);
+//			} else if (type2.equals("-")) {
+//				minus(sum, num3);
+//			}
+//		}
+//
+//		scan.close();
+//	}
 
-	public static void minus(int n1, int n2) {
-		System.out.println(n1 -n2);
-	}
 
+//	リファクタリング
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String f = scan.nextLine();
-
 		String[] nums = f.split(" ");
-		int num1 = Integer.parseInt(nums[0]);
-		String type1 = nums[1];
-		int num2 = Integer.parseInt(nums[2]);
-		String type2 = nums[3];
-		int num3 = Integer.parseInt(nums[4]);
 
-		if (type1.equals("+")) {
-			int sum = num1 + num2;
-			if (type2.equals("+")) {
-				plus(sum, num3);
-			} else if (type2.equals("-")) {
-				minus(sum, num3);
-			}
-		} else if (type1.equals("-")) {
-			int sum = num1 - num2;
-			if (type2.equals("+")) {
-				plus(sum, num3);
-			} else if (type2.equals("-")) {
-				minus(sum, num3);
+		String type = null;
+		int sum = 0;
+
+		for (int i = 0; i < nums.length; i++) {
+			if(nums[i].equals("+") || nums[i].equals("-")) {
+				type = nums[i];
+			} else {
+				int num = Integer.parseInt(nums[i]);
+				if (type != null) {
+					if (type.equals("+")) {
+						sum += num;
+					} else if (type.equals("-")) {
+						sum -= num;
+					}
+				} else {
+					sum = num;
+				}
 			}
 		}
-
-		scan.close();
+		System.out.println(sum);
 	}
 }
