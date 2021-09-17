4a0de119e9fb3f6a2ae69dcd26cc59234a45e783
terakawaakari <terakawaakari0701@gmail.com>
2021-09-17 23:54:46 +0900

    learn lamda style

---
+++ src/lambdaStyle/Greeting.java
@@ -0,0 +1,17 @@
+package lambdaStyle;
+
+@FunctionalInterface
+public interface Greeting {
+	public void hello(String word);
+}
+
+class Main {
+	public static void main(String[] args) {
+		Greeting greeting = (String word) -> {
+			System.out.println(word);
+		};
+
+		Greeting greeting2 = System.out::println;
+	}
+}
+
