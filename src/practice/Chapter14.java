ee54f3ea1e648d0815afadc8b16d092d375702d4
terakawaakari <terakawaakari0701@gmail.com>
2021-09-20 23:41:47 +0900

    learn stream

--- src/practice/Chapter14.java
+++ src/practice/Chapter14.java
@@ -1,6 +1,9 @@
 package practice;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Chapter14 {
@@ -16,5 +19,28 @@ public class Chapter14 {
 			int hp = character.attack();
 			System.out.println("ヒットポイント：" + hp);
 		}
+		
+		List<Integer> numbers = Arrays.asList(3,1,2,4);
+		List<String> strs = Arrays.asList("a", "c", "b");
+		
+		Collections.sort(
+				numbers,
+				new Comparator<Integer>() {
+					@Override
+					public int compare(Integer x, Integer y) {
+						return y - x;
+					}
+				});
+		System.out.println(numbers);
+		
+		Collections.sort(
+				strs,
+				new Comparator<String>() {
+					@Override
+					public int compare(String x, String y) {
+						return x.compareTo(y);
+					}
+				});
+		System.out.println(strs);
 	}
 }
